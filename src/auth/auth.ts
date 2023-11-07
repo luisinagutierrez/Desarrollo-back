9efaabcc2ce1c8dd37e9f8c612b16f56bf6d1f0c
Luca Gini <luca.giniclc@gmail.com>
2023-11-07 14:07:42 -0300

    added auth middleware but something is not working properly

---
+++ src/auth/auth.ts
@@ -0,0 +1,35 @@
+// import pkg from 'jsonwebtoken';
+// import {config} from 'dotenv';
+// import { NextFunction } from 'express';
+// import { User } from '../user/user.entity.js';
+
+// config();
+
+// const jwt = pkg;
+
+// const secret = process.env.SECRET;
+
+// export const generateToken = (user: User) =>{
+//   if (secret) {
+//     return jwt.sign(user, secret, {expiresIn: '10m'});
+//   } else {
+//     throw new Error('Secret not found');
+//   }
+//   }
+
+// export const validateToken = (req: Request, res: Response, next: NextFunction) =>{
+//   const authHeader = req.headers['authorization'];
+
+// if(authHeader){
+//   const token = authHeader.split(' ')[1];
+//   jwt.verify(token, secret, (err, user) =>{
+//     if(err){
+//       return res.status(401).json('Token inválido'  );
+//     } else{
+//       next();
+//     }
+//   });
+// } else{
+//   return res.status(401).json('Token no proporcionado');
+// }
+// }
