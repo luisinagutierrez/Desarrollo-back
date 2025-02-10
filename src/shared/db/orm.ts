2153342756c67131b32b36af71fac66c334fa33f
luisinagutierrez <luisinagutierrez2618@gmail.com>
2025-02-10 11:45:26 -0300

    integration test data base created

--- src/shared/db/orm.ts
+++ src/shared/db/orm.ts
@@ -1,11 +1,19 @@
 import { MikroORM, t } from "@mikro-orm/core";
 import { MongoHighlighter } from "@mikro-orm/mongo-highlighter";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const DB_NAME =
+  process.env.NODE_ENV === "integrationtest"
+    ? process.env.DB_NAME_TEST
+    : process.env.DB_NAME_DEV;
 
 export const orm = await MikroORM.init({
   entities: ['dist/**/*.entity.js'],
   entitiesTs: ['src/**/*.entity.ts'],
-  dbName: 'Ecommerce2023',
+  //dbName: 'Ecommerce2023',
+  dbName: DB_NAME,
   type: 'mongo',
   clientUrl: 'mongodb://127.0.0.1:27017',
   highlighter: new MongoHighlighter(),
