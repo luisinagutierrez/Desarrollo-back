9de2909b6409c4ba520d22fe9f620bfeb7288712
luisinagutierrez <luisinagutierrez2618@gmail.com>
2025-02-20 11:29:51 -0300

    Restrict administrator access to the user information component

--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { controller } from "./user.controller.js";
-import { authenticateAdmin } from "../auth/authMiddleware.js";
 import { authenticateClient } from "../auth/authMiddleware.js";
 
 export const userRouter = Router();
@@ -10,5 +9,5 @@ userRouter.put('/update-password', controller.updatePassword);
 userRouter.get('/by-email', controller.findUserByEmail);
 userRouter.delete('/:id', authenticateClient, controller.remove);
 userRouter.get('/:id', controller.findOne);
-userRouter.put('/:id', controller.update);
+userRouter.put('/:id',authenticateClient, controller.update);
 userRouter.post('/', controller.signUp);
